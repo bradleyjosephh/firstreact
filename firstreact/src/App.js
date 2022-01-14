0b7d2a8d853e2c0860d2410763e0f93659d0484b
bradleyjosephh <bradleyjosephh@gmail.com>
2022-01-14 13:26:27 -0800

    display object parts on page

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,5 +1,7 @@
 import Navbar from './components/Navbar/Navbar.js'
 
+const movie = {"Title":"Goodfellas","Year":"1990","Rated":"R","Released":"21 Sep 1990","Runtime":"146 min","Genre":"Biography, Crime, Drama","Director":"Martin Scorsese","Writer":"Nicholas Pileggi, Martin Scorsese","Actors":"Robert De Niro, Ray Liotta, Joe Pesci","Plot":"The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.","Language":"English, Italian","Country":"United States","Awards":"Won 1 Oscar. 44 wins & 38 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"8.7/10"},{"Source":"Rotten Tomatoes","Value":"96%"},{"Source":"Metacritic","Value":"90/100"}],"Metascore":"90","imdbRating":"8.7","imdbVotes":"1,086,143","imdbID":"tt0099685","Type":"movie","DVD":"22 Aug 1997","BoxOffice":"$46,836,214","Production":"N/A","Website":"N/A","Response":"True"}
+
 const App = () => {
   return (
     <>
@@ -17,6 +19,14 @@ const App = () => {
       I like to eat.
     </li>
     </ul>
+    <div class="card">
+  <div class="card-body">
+  <p> Title: {movie.Title}</p>
+    <p> Year: {movie.Year}</p>
+    <p> Rated: {movie.Rated}</p>
+    <p> Rated: {movie.Genre}</p>
+  </div>
+  </div>
     <img src="https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/d0ccx26lkyay9dao_1615803365.jpeg" alt="aot_pic"></img>
     </div>
     </>
