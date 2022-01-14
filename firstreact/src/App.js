d638eb58d3957c9b95d8b20656a75d548cd9220e
bradleyjosephh <bradleyjosephh@gmail.com>
2022-01-14 13:29:21 -0800

    navbar title displaying

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -2,10 +2,13 @@ import Navbar from './components/Navbar/Navbar.js'
 
 const movie = {"Title":"Goodfellas","Year":"1990","Rated":"R","Released":"21 Sep 1990","Runtime":"146 min","Genre":"Biography, Crime, Drama","Director":"Martin Scorsese","Writer":"Nicholas Pileggi, Martin Scorsese","Actors":"Robert De Niro, Ray Liotta, Joe Pesci","Plot":"The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.","Language":"English, Italian","Country":"United States","Awards":"Won 1 Oscar. 44 wins & 38 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"8.7/10"},{"Source":"Rotten Tomatoes","Value":"96%"},{"Source":"Metacritic","Value":"90/100"}],"Metascore":"90","imdbRating":"8.7","imdbVotes":"1,086,143","imdbID":"tt0099685","Type":"movie","DVD":"22 Aug 1997","BoxOffice":"$46,836,214","Production":"N/A","Website":"N/A","Response":"True"}
 
+
+const title = "Super sick man"
+
 const App = () => {
   return (
     <>
-    <Navbar />
+    <Navbar title={title} />
     <div>
     <h1>Bradley Bio</h1>
     <ul>
@@ -19,8 +22,8 @@ const App = () => {
       I like to eat.
     </li>
     </ul>
-    <div class="card">
-  <div class="card-body">
+    <div className="card">
+  <div className="card-body">
   <p> Title: {movie.Title}</p>
     <p> Year: {movie.Year}</p>
     <p> Rated: {movie.Rated}</p>
--- firstreact/src/components/Navbar/Navbar.js
+++ firstreact/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = props => {
     console.log(props)
     return(
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a classNameName="navbar-brand" href="/">{props.Title}</a>
+  <a classNameName="navbar-brand" href="/">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
