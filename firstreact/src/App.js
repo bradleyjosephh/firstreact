4d5dbf64067dbee33e193fc8cb947eb32ea01d5d
bradleyjosephh <bradleyjosephh@gmail.com>
2022-01-14 11:27:59 -0800

    bio created in react

---
+++ firstreact/src/App.js
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div>
+    <h1>Bradley Bio</h1>
+    <ul>
+    <li>
+      I like to surf.
+    </li>
+    <li>
+      I like to snowboard.
+    </li>
+    <li>
+      I like to eat.
+    </li>
+    </ul>
+    <img src="https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/d0ccx26lkyay9dao_1615803365.jpeg" alt="aot_pic"></img>
+    </div>
+  )
+}
+
+export default App
